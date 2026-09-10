ed47455b29c5987093b5a2c1145b3f38b26fbc83
kanvipanchal-atharva <kanvipanchal@atharvacoe.ac.in>
2026-09-10 16:35:48 +0530

    udpate conneted given by teh varsha ma'am

--- frontend/components/about/TeamSection.jsx
+++ frontend/components/about/TeamSection.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const teamMembers = [
   { id: "sunil-rane", name: "Mr. Sunil Rane", role: "Founder", image: "/Images/about/founder.png" },
-  { id: "varda", name: "Varda", role: "Design Head", image: "/Images/about/head.jpeg" },
+  { id: "varda", name: "Varda", role: "Lead Designer", image: "/Images/about/head.jpeg" },
 ];
 
 export default function TeamSection() {
