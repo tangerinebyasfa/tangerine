b85e9d471c13f8ee155e934ef2bab0bd625d0fb5
kanvipanchal-atharva <kanvipanchal@atharvacoe.ac.in>
2026-09-22 17:45:48 +0530

    outlet page

--- frontend/app/products/[category]/page.js
+++ frontend/app/products/[category]/page.js
@@ -55,7 +55,7 @@ const OUTLET_LOCATIONS = [
     ],
     phone: "+91 94211 74383",
     whatsapp: "+91 94211 74383",
-    hours: ["Mon - Sat: 11:00 AM - 8:30 PM"],
+    hours: ["Mon - Sun: 11:00 AM - 8:30 PM"],
     mapSrc:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3811.80749379707!2d73.25541007607045!3d17.179573108756795!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bea0d22b314f62f%3A0xd7222cf5ca7e771b!2sBlue%20Ocean%20The%20Fern%20Resort%20%26%20Spa%20Ganpatipule%2C%20Series%20by%20Marriott!5e0!3m2!1sen!2sin!4v1788167283728!5m2!1sen!2sin",
   },
